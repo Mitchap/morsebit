d20a0448b95ec1c1bc850b2b2a84d61fd801f59b
Mitchap <mitchbarcenilla4@gmail.com>
2024-04-22 19:35:26 +0800

    price currency updated

--- src/pages/Collections.vue
+++ src/pages/Collections.vue
@@ -47,7 +47,7 @@
             <p
               class="mt-1 text-lg font-medium font-mono text-orange-500 text-decoration"
             >
-            <span>&#8369</span>{{ product.price }}
+              {{ product.price }}
             </p>
           </router-link>
         </div>
